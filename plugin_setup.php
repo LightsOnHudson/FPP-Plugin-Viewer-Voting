481db9057757bce55b50dc173275efed97a94e96
LightsOnHudson <bshaver@MBAIR-TDL.home.lan>
2017-08-03 06:55:58 -0600

    Update

--- plugin_setup.php
+++ plugin_setup.php
@@ -71,7 +71,7 @@ if(isset($_POST['sync_sequnces'])) {
 	//the sync sequences was pressed
 	logEntry("Syncing playlists");
 	
-	$PLAYLIST_DIR = $settings['mediaDirectory']."playlists/";
+	$PLAYLIST_DIR = $settings['mediaDirectory']."/playlists/";
 	logEntry("playlist directory: ".$PLAYLIST_DIR);
 	$PLAYLIST_ARRAY = directoryToArray($PLAYLIST_DIR, $recursive);
 	
